f4499761f6abbb3be3e95a0d2f1856765ad7cddc
msparrowskyIT <mwroblewski.pw@gmail.com>
2018-12-06 14:15:25 +0100

    Improve edge representation

--- edge.py
+++ edge.py
@@ -1,18 +1,28 @@
 from point import *
 
 class Edge:
-    "Representation of single edge in 2D coordinate system."
+    "Representation of single edge in 3D/2D coordinate system."
 
     def __init__(self, points3D, distance):
         self.points3D = points3D
         self.coefs3D = self.__coefs3D()
-        self.points2D = tuple(p.to2D(distance) for p in points3D)
+        self.points2D = self.__points2D(self.points3D, distance)
         self.coefs2D = self.__coefs2D()
 
     def __repr__(self):
         return f"""Edge:
-        3D: {self.points3D}, x = {self.points3D[0].x} + {self.coefs3D['a']}*t, y = {self.points3D[0].y} + {self.coefs3D['b']}*t, z = {self.points3D[0].z} + {self.coefs3D['c']}*t.
-        2D: {self.points2D}, y = {self.coefs2D['a']}*x + {self.coefs2D['b']}"""
+        \t3D: {self.points3D}, x = {self.points3D[0].x} + {self.coefs3D['a']}*t, y = {self.points3D[0].y} + {self.coefs3D['b']}*t, z = {self.points3D[0].z} + {self.coefs3D['c']}*t.
+        \t2D: {self.points2D}, y = {self.coefs2D['a']}*x + {self.coefs2D['b']}"""
+
+    def __points2D(self, points3D, distance):
+        """Calculate edge's representation in 2D coordinate system.
+        If any of points is behind observer, the 2D representation doesn't exist."""
+        try:
+            points2D = tuple(p.to2D(distance) for p in points3D)
+        except PointBehindObserver:
+            points2D = None
+
+        return points2D
 
     def __coefs3D(self):
         "Calculate edge's 3D coefficients: x = x0 + a*x, y = y0 + b*y, z = z0 + c*z."
@@ -24,13 +34,22 @@ class Edge:
 
     def __coefs2D(self):
         "Calculate edge's 2D coefficients: y = a*x + b."
-        try:
-            a = (self.points3D[1].y - self.points3D[0].y) / (self.points3D[1].x - self.points3D[0].x)
-        except ZeroDivisionError:
-            a = 0
-        b = self.points3D[1].y - a * self.points3D[1].x
+        if(not self.points2D):
+            return None
+        else:
+            try:
+                a = (self.points2D[1].y - self.points2D[0].y) / (self.points2D[1].x - self.points2D[0].x)
+            except ZeroDivisionError:
+                a = 0
+            b = self.points2D[1].y - a * self.points2D[1].x
+            
+            return {'a': a, 'b': b}
 
-        return {'a': a, 'b': b}
+    def __update(self, distance=1):
+        "Update edge's coeffitients and 2D representation in context of changing points3D."
+        self.coefs3D = self.__coefs3D()
+        self.points2D = self.__points2D(self.points3D, distance)
+        self.coefs2D = self.__coefs2D()
 
     def cross3D(self, value, axis):
         "Return Point3D if parameter value is in edge's range."
@@ -63,8 +82,6 @@ class Edge:
 
         return cross(value) if cross else None
         
-
-
     def cross2D(self, value, axis):
         "Return Point2D if parameter value is in edge's range."
         def x_cross(x):
@@ -78,18 +95,27 @@ class Edge:
                 return Point2D((y - self.coefs2D['b']) / self.coefs2D['a'], y)
             
             return None
+        
+        if(not self.points2D):
+            return None
+        else:
+            cross = {
+                'x': x_cross,
+                'y': y_cross
+            }.get(axis)
+            
+            return cross(value) if self.points2D and cross else None
 
-        cross = {
-            'x': x_cross,
-            'y': y_cross
-        }.get(axis)
+    def move(self, axis, step, distance):
+        "Move edge in 3D coordinate system and update coefficients and 2D representation."
+        list(map(lambda p: p.move(axis, step), self.points3D))
+        self.__update(distance)
+        
+    def rotate(self, axis, angle, distance):
+        "Rotate edge in 3D coordinate system and update coefficients and 2D representation."
+        self.points3D = tuple(p.rotate(axis, angle) for p in self.points3D)
+        self.__update(distance)
 
-        return cross(value) if cross else None
-
-
-p1 = Point3D(1, 1, 1)
-p2 = Point3D(2, 2, 1)
-
-e = Edge((p1, p2), 1)
-print(e)
-print(e.cross2D(1.5, 'y'))
+    def zoom(self, distance):
+        "Zoom edge in 3D coordinate system and update coefficients and 2D representation."
+        self.__update(distance)

4f58e6302ae99093bedd42c4bce58cc9d6f2ee5e
msparrowskyIT <mwroblewski.pw@gmail.com>
2018-12-07 09:08:54 +0100

    Prim made ud of sides.

--- prim.py
+++ prim.py
@@ -1,54 +1,79 @@
 from point import Point3D, PointBehindObserver
+from edge import *
+from side import *
 
 class Prim:
     "Representation of prime in 3d coordinate system"
-    def __init__(self, point, width, height, deepth, color = 'red'):
-        # front points
-        self.points = [point]
-        self.points.append(Point3D(point.x + width, point.y, point.z))
-        self.points.append(Point3D(point.x + width, point.y + height, point.z))
-        self.points.append(Point3D(point.x, point.y + height, point.z))
-        #back points
-        self.points.append(Point3D(point.x, point.y, point.z + deepth))
-        self.points.append(Point3D(point.x + width, point.y, point.z + deepth))
-        self.points.append(Point3D(point.x + width, point.y + height, point.z + deepth))
-        self.points.append(Point3D(point.x, point.y + height, point.z + deepth))
+    
+    def __init__(self, point, width, height, deepth, distance, color = 'red'):
+        points3D = self.__create_points3D(point, width, height, deepth)
+        edges = self.__create_edges(points3D, distance, color)
+        self.sides = self.__create_sides(edges, color)
         self.color = color
 
-    def __repr__(self):
-        return f"Prim: {self.points}"
+    def __create_points3D(self, point3D, width, height, deepth):
+        """Create prime's points3D."""
+        # front: left bottom, right bottom, right top, left top points
+        points3D = [point3D]
+        points3D.append(Point3D(point3D.x + width, point3D.y, point3D.z))
+        points3D.append(Point3D(point3D.x + width, point3D.y + height, point3D.z))
+        points3D.append(Point3D(point3D.x, point3D.y + height, point3D.z))
+        #back: left bottom, right bottom, right top, left top points
+        points3D.append(Point3D(point3D.x, point3D.y, point3D.z + deepth))
+        points3D.append(Point3D(point3D.x + width, point3D.y, point3D.z + deepth))
+        points3D.append(Point3D(point3D.x + width, point3D.y + height, point3D.z + deepth))
+        points3D.append(Point3D(point3D.x, point3D.y + height, point3D.z + deepth))
 
-    def change_color(self, color):
-        self.color = color
+        return points3D
 
-    def move(self, axis, step):
-        list(map(lambda point: point.move(axis, step), self.points))
+    def __create_edges(self, points3D, distance, color):
+        """Create prime's edges."""
+        def create_edge(i, j):
+            return Edge((points3D[i], points3D[j]), distance, color)
+        
+        # front: bottom, right, top, left edges    
+        edges = [create_edge(0, 1)]
+        edges.append(create_edge(1, 2))
+        edges.append(create_edge(2, 3))
+        edges.append(create_edge(3, 0))
+        # back: bottom, right, top, left edges
+        edges.append(create_edge(4, 5))
+        edges.append(create_edge(5, 6))
+        edges.append(create_edge(6, 7))
+        edges.append(create_edge(7, 4))
+        # side: left bottom, right bottom, right top, left top edges
+        edges.append(create_edge(0, 4))
+        edges.append(create_edge(1, 5))
+        edges.append(create_edge(2, 6))
+        edges.append(create_edge(3, 7))
 
-    def rotate(self, axis, angle):
-        list(map(lambda point: point.rotate(axis, angle), self.points))
+        return edges
 
-    def __get_poligons3D(self):
-        poligons3D = []
-        # front wall
-        poligons3D.append((self.points[0], self.points[1], self.points[2], self.points[3]))
-        # back wall
-        poligons3D.append((self.points[4], self.points[5], self.points[6], self.points[7]))
-        # side walls
-        poligons3D.append((self.points[0], self.points[1], self.points[5], self.points[4]))
-        poligons3D.append((self.points[1], self.points[2], self.points[6], self.points[5]))
-        poligons3D.append((self.points[2], self.points[3], self.points[7], self.points[6]))
-        poligons3D.append((self.points[0], self.points[3], self.points[7], self.points[4]))
-        return poligons3D
+    def __create_sides(self, edges, color):
+        """Create prime's sides."""
+        def create_side(i, j, k, l):
+            return Side((edges[i], edges[j], edges[k], edges[l]), color)
 
-    def get_poligons2D(self, distance):
-        poligons3D = self.__get_poligons3D()
-        poligons2D = []
-        for poligon3D in poligons3D:
-            try:
-                points2D = tuple(map(lambda point3D: point3D.to2D(distance), poligon3D))
-                poligons2D.append(points2D)
-            except PointBehindObserver:
-                pass
-                #print(f"Poligon: {poligon3D} contains point behind observer.")
+        # front, back sides 
+        sides = [create_side(0, 1, 2, 3)]
+        sides.append(create_side(4, 5, 6, 7))
+        # top, bottom sides 
+        sides.append(create_side(2, 10, 6, 11))
+        sides.append(create_side(0, 9, 4, 8))
+        # left, right sides
+        sides.append(create_side(8, 7, 11, 3))
+        sides.append(create_side(9, 5, 10, 1))
 
-        return poligons2D
+        return sides
+
+    def move(self, axis, step, distance):
+        "Move prim in 3D coordinate system."
+        list(map(lambda s: s.move(axis, step, distance), self.sides))
+        
+    def rotate(self, axis, angle, distance):
+        "Rotate prim in 3D coordinate system."
+        list(map(lambda s: s.rotate(axis, angle, distance), self.sides))
+
+    def zoom(self, distance):
+        "Zoom prim in 2D coordinate system."
+        list(map(lambda s: s.zoom(distance), self.sides))
